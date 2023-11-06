dcf54d96b721edd74a64e47a2b59ab6cec064c7b
Erik-Mfa <erikmfa190230@gmail.com>
2023-11-06 21:39:25 +1100

    database connection / login / admin & customer main page

---
+++ database/conn.php
@@ -0,0 +1,8 @@
+<?php  
+    include_once 'config.php';
+
+    function connect()
+    {
+        return mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME);
+    }
+?>
---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <nav>
+        <h1>Login</h1>
+    </nav>
+</body>
+
+<form action="./index.php" method="post">
+    <div>
+        <label for="user"></label>
+        <input type="text" name="user">
+    </div>
+
+    <div>
+        <input type="submit" value="send">
+    </div>
+</form>
+
+</html>
